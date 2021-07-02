4a470b9cfdf5cff93ab9513fb69065e381ccd4a6
Matthias <mprost@scaleway.com>
2021-07-02 12:16:06 +0200

    fix: make find and replace gz file recursively

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,9 +58,7 @@ if [ "$INPUT_COMPRESS_TOOL" != "" ] && [ "$INPUT_COMPRESS_TOOL" = "gzip" ]; then
       gzip -9 -r "${INPUT_SOURCE_DIRECTORY}"
 
       # Rename all *.gz to original name
-      for f in ${INPUT_SOURCE_DIRECTORY}/*.gz; do
-          mv -- "$f" "${f%.gz}"
-      done
+      find ./out -name "*.gz" -exec sh -c 'mv "$0" "${0%.gz}"' {} \;
 
       HEADER="--content-encoding=$INPUT_COMPRESS_TOOL"
 fi
